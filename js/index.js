a97e67304defcbfe0e0dbcd543b3caf12127e6ba
Carlos Parodi <se.phoenix@gmail.com>
2021-10-30 01:22:15 +0200

    primera parte completa

--- js/index.js
+++ js/index.js
@@ -3,18 +3,37 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  let price = product.querySelector('.price span');
+  let quantity = product.querySelector('.quantity input');
+  let subtotal = product.querySelector('.subtotal span');
+  
+  let subtotalprice = price.innerHTML * quantity.value;
+  subtotal.innerHTML = subtotalprice;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+ /* const singleProduct = document.querySelector('.product');
+  updateSubtotal(singleProduct);*/
   // end of test
+  let tbodyjs = document.querySelector('tbody');
+  let hilera = document.createElement('tr');
+  let celda = document.createElement('td');
+  let textoCelda = document.createTextNode('textoCelda');
+  
 
-  // ITERATION 2
-  //... your code goes here
+  celda.appendChild(textoCelda);
+  hilera.appendChild(celda);
+  hilera.appendChild(celda);
+  tbodyjs.appendChild(hilera);
+  
+  celda.appendChild(textoCelda);
+  hilera.appendChild(celda);
+  hilera.appendChild(celda);
+  tbodyjs.appendChild(hilera);
+  
+  
 
   // ITERATION 3
   //... your code goes here
